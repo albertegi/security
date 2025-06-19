379168c63cf0d3c1481a9be348add24913f68fc0
albertegi <albertegi90@gmail.com>
2025-06-19 04:54:20 +0100

    update JwtService with SigningKey

--- src/main/java/com/alvirg/security/config/JwtService.java
+++ src/main/java/com/alvirg/security/config/JwtService.java
@@ -1,11 +1,33 @@
 package com.alvirg.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "369ac70b3764262f3c2a1dbba454483f51a4fdb39d2a5233836fcf1a75328975";
 
     public String extractUserName(String jwtToken) {
         return null;
+    }
+
+    // a method to extract claims
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
